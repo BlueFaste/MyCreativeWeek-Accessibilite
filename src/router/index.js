fa7e4eaf5b3fcc67db600f79f5102eb9c6e1df61
Caroline Fassot <fassot.caroline@gmail.com>
2020-11-30 12:09:51 +0100

    add views and components

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,24 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+    {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+    {
+    path: '/project',
+    name: 'project',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Project.vue')
+  },
+
 ]
 
 const router = new VueRouter({
