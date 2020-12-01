deccdb225f3cf1bfb323c453763419518c4c37f5
Caroline Fassot <fassot.caroline@gmail.com>
2020-12-01 14:51:03 +0100

    footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
     <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 
+
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default {
   name: 'Home',
   components: {
+    Footer,
     Header
+
   }
 }
 </script>
