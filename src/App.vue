1e0907c42c3831d624068f5df622951bd8639e01
Caroline Fassot <fassot.caroline@gmail.com>
2020-12-01 11:49:41 +0100

    fonts plus color black

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="px-10rem">
+    <!--    <div id="nav">-->
+    <!--      <router-link to="/">Home</router-link> |-->
+    <!--      <router-link to="/about">About</router-link>-->
+    <!--    </div>-->
     <router-view/>
   </div>
 </template>
@@ -28,5 +28,55 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+
+.px-10rem {
+  padding: 0 10rem;
+}
+
+@font-face {
+  font-family: "ubuntu";
+  font-style: normal;
+  src: url("assets/fonts/Ubuntu-Regular.ttf");
+}
+
+@font-face {
+  font-family: "ubuntu-bold";
+  src: url("assets/fonts/Ubuntu-Bold.ttf");
+}
+
+@font-face {
+  font-family: "dinot-regular";
+  font-style: normal;
+  src: url("assets/fonts/dinot.otf");
+}
+
+@font-face {
+  font-family: "dinot-bold";
+  font-style: normal;
+  src: url("assets/fonts/DINOT-Bold.otf");
+}
+
+.ubuntu {
+  font-family: ubuntu;
+}
+
+.ubuntu-B{
+  font-family: ubuntu-bold;
+}
+
+
+.dinot {
+  font-family: dinot-regular;
+}
+
+.dinot-B {
+  font-family: dinot-bold;
+}
+
+.blackMDS{
+  background-color:#1D1D1D !important;
+  opacity: 80% !important;
 }
 </style>
