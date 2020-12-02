b829a7a49c2c310c047489b4c7837a7fc6d3223f
Caroline Fassot <fassot.caroline@gmail.com>
2020-12-02 16:22:17 +0100

    change logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 	<header class="d-flex justify-content-between ubuntu-B align-items-center py-2 px-10rem" role="banner">
 		<h1 class="projects-title">
-			<span lang="en"><img src="../assets/LOGO_MDS.png" alt="MyDigitalSchool" id="logoMDS"></span><span> / Projets</span>
+			<img src="../assets/logo-MyDigitalProject.png" alt="MyDigitalProject" id="logoMDS">
 		</h1>
 		<div>
 			<b-button v-show="!login" class="blackMDS buttonMDS">Connexion</b-button>
