3ecd9bc1355e23ac2931fe3e6ae4df80470137b5
Caroline Fassot <fassot.caroline@gmail.com>
2020-12-02 17:22:07 +0100

    ajout color tags

--- src/components/ProjectDisplay.vue
+++ src/components/ProjectDisplay.vue
@@ -1,13 +1,15 @@
 <template>
-	<div class="projectSquare text-white d-flex flex-column justify-content-end align-items-start" :id="projetTitle"  :style="{ backgroundImage: `url(${imgUrl})`}" tabindex="0">
+	<div class="projectSquare text-white d-flex flex-column justify-content-end align-items-start" :id="projetTitle" :style="{ backgroundImage: `url(${imgUrl})`}" tabindex="0">
 		<div class="blackFiltre" v-if="filterBlack"></div>
 		<div class="whiteFiltre" v-else></div>
-		<h3 class="text-left w-75">{{projetTitle}}</h3>
-		<div class="d-flex w-100">
-		<span :class="`projectTag ${ecole}`">{{ecoleRewrite}}</span>
-		<span :class="`projectTag ${formation}`">{{formationRewrite}}</span>
+		<h3 class="text-left w-75">{{ projetTitle }}</h3>
+		<div class="d-flex w-100 align-items-center ">
+			<span :class="`projectTag ${ecole}`">{{ ecoleRewrite }}</span>
+			<span :class="`projectTag ${formation} `">{{ formationRewrite }}</span>
 		</div>
-		<b-button class="align-self-end buttonVoirPlus text-dark">Voir plus</b-button>
+    <router-link :to="link" class="align-self-end buttonVoirPlus text-dark" :title="'Voir plus - ' + projetTitle">
+     Voir plus
+    </router-link>
 	</div>
 </template>
 
@@ -16,11 +18,12 @@
 
 export default {
 	name: "ProjectDiplay",
-	props:{
-		projetTitle : String,
+	props: {
+		projetTitle: String,
 		imgUrl: String,
 		ecole: String,
 		formation: String,
+    link: String,
 		filterBlack: Boolean,
 	},
 	mounted() {
@@ -28,15 +31,15 @@ export default {
 		const arrayFormation = this.formation.split("_")
 
 
-		for (const word in arrayEcole){
+		for (const word in arrayEcole) {
 			this.ecoleRewrite = this.ecoleRewrite + arrayEcole[word] + ' '
 		}
-		for (const word in arrayFormation){
+		for (const word in arrayFormation) {
 			this.formationRewrite = this.formationRewrite + arrayFormation[word] + ' '
 		}
 	},
-	data(){
-		return{
+	data() {
+		return {
 			ecoleRewrite: "",
 			formationRewrite: "",
 		}
@@ -45,7 +48,7 @@ export default {
 </script>
 
 <style scoped>
-.projectSquare{
+.projectSquare {
 	position: relative;
 	background-size: cover;
 	background-position: center;
@@ -53,25 +56,27 @@ export default {
 	width: 33%;
 }
 
-.projectTag, .buttonVoirPlus, h3{
+.projectTag, .buttonVoirPlus, h3 {
 	z-index: 10;
 }
-.projectTag:first-child, h3{
+
+.projectTag:first-child, h3 {
 	z-index: 10;
 	margin-left: 10% !important;
 }
 
-h3{
+h3 {
 	font-size: 1.2rem;
 }
 
-.projectTag{
+.projectTag {
 	border-radius: 20px;
 	padding: 0.2rem 0.7rem;
 	margin: 1rem 0.5rem;
+  height: max-content;
 }
 
-.buttonVoirPlus, .buttonVoirPlus:hover, .buttonVoirPlus:focus{
+.buttonVoirPlus, .buttonVoirPlus:hover, .buttonVoirPlus:focus {
 	background-color: white;
 	border: none;
 	border-radius: 0px;
@@ -81,19 +86,94 @@ h3{
 	padding: 0.2rem 0.5rem;
 }
 
-.Angers{
-	background-color: #D83A3A;
+.Inter-école{
+  background-color: #005757;
 }
 
-.Paris{
-	background-color: #4D7E2F ;
+.Angers {
+	background-color: #A72B00;
+}
+
+.Paris {
+	background-color: #D3273E;
+}
+
+.Annecy {
+	background-color: #5D3754;
+}
+
+.Bordeaux {
+	background-color: #007A78;
+}
+
+.Caen {
+	background-color: #BE0D0D;
+}
+
+.Grenoble {
+	background-color: #7A4600;
+}
+
+.Lille {
+	background-color: #973094;
+}
+
+.Lyon {
+	background-color: #4F3097;
+}
+
+.Melun {
+	background-color: #304697;
+}
+
+.Montpellier {
+	background-color: #4B60A0;
+}
+
+.Nantes {
+	background-color: #A04B4C;
+}
+
+.Nice {
+	background-color: #197BB8;
+}
+
+.Rennes {
+	background-color: #2D7B36;
+}
+
+.Saint-Quentin {
+	background-color: #4C2CA3;
+}
+
+.Toulouse {
+	background-color: #2A5B4A;
+}
+
+.Vannes {
+	background-color: #6D7500;
+}
+
+.Bachelor_1{
+  background-color: #193171;
 }
 
 .Bachelor_2 {
-background-color: #AE6300;
+	background-color: #1F6FBF;
 }
 
-.B3_e-business{
-	background-color: #0478A2;
+.B3_Webdesign,
+.B3_e-Business ,
+.B3_Développeur_Web,
+.B3_Webmarketing{
+  background-color: #4529B9;
 }
+
+.MBA_Marketing_Digital, .MBA_Data_Marketing,
+.MBA_UX-UI_design,
+.MBA_Développeur_full-stack,
+.MBA_Management_Digital{
+  background-color: #487C1B;
+}
+
 </style>
